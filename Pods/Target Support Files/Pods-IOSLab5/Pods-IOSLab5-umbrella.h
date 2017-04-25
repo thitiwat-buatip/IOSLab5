8a4750f0c2831041f4ffadcfe24b56df0ed54baf
thitiwat-buatip <Subin@Thitiwats-Mac.local>
2017-04-25 19:58:44 +0700

    Upload File

---
+++ Pods/Target Support Files/Pods-IOSLab5/Pods-IOSLab5-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_IOSLab5VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_IOSLab5VersionString[];
+
